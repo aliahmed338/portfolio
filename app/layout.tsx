4215c802c84bcefee4e60bfaf3abb352154641c8
Ali <aliahmed60345@gmail.com>
2025-03-25 04:00:46 +0200

    update project add icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Work_Sans } from "next/font/google";
 import "./globals.css";
-import "../public/Ali.png";
 
 const workSans = Work_Sans({
   subsets: ["latin"],
